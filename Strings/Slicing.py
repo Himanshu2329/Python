b6114f61649c781aa6f799bb3def7138f76f3f93
Himanshu2329 <kumarhimanshu4390@gmail.com>
2023-01-07 00:26:36 +0530

    slicing started

---
+++ Strings/Slicing.py
@@ -0,0 +1,16 @@
+# concatination
+
+grerting="Good Morning "
+name="himanshu"
+c=grerting+name
+print(c)
+print(type(name))
+
+
+# Slicing
+name="himanshu"
+print(name[-5])
+# name[2]="da" #--> it does not work
+
+print(name[0:4])
+print(name[-7:-1])
