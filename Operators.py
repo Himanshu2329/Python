b06151cc2ba4c98476e05d0bd03588da995ac0e9
Himanshu2329 <kumarhimanshu4390@gmail.com>
2023-01-06 23:40:17 +0530

    Operators done

---
+++ Operators.py
@@ -0,0 +1,18 @@
+a=10
+b=199
+print("the value of a+b",a+b)
+
+a+=2
+print(a)
+
+c=(19<18)
+print(c)
+
+# logical operator
+
+bool1=True
+boo2=False
+
+print("The value of bool1 and bool2 is",(bool1 and boo2))
+print("The value of bool1 or bool2 is",(bool1 or boo2))
+print("The value of bool1 not bool2 is",(not  boo2))
