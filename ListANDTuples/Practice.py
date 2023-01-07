aec5f82ca59059926e91a21d1a57a1a3eaaa0b0a
Himanshu2329 <kumarhimanshu4390@gmail.com>
2023-01-08 00:29:18 +0530

    List and Tuples Done

---
+++ ListANDTuples/Practice.py
@@ -0,0 +1,25 @@
+# Question 1
+f1=input("enter fruit\n")
+f2=input("enter fruit\n")
+f3=input("enter fruit\n")
+f4=input("enter fruit\n")
+f5=input("enter fruit\n")
+
+t1=(f1,f2,f3,f4,f5)
+print(t1)
+
+# Question 2
+m1=int(input("Enter marks: "))
+m2=int(input("Enter marks: "))
+m3=int(input("Enter marks: "))
+m4=int(input("Enter marks: "))
+
+l1=[m1,m2,m3,m4]
+l1.sort()
+print(l1)
+
+# Question 4
+
+print(l1[0]+l1[1]+l1[2]+l1[3])
+print(sum(l1))
+
