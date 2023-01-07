1ce3d9b6f1ef9983a89710a09c966363787f535f
Himanshu2329 <kumarhimanshu4390@gmail.com>
2023-01-08 00:19:27 +0530

    Tuples methods started

---
+++ ListANDTuples/Tuples.py
@@ -0,0 +1,8 @@
+# you can update the values of tuples
+t=(1,2,3,4)
+# t1=() # empty tuple
+t1=(1,) # if we want to add only one element in the tuple just make sure to put a coma after the element
+
+print(t1)
+# t[2]=1
+print(t[2])
