f7b9fc85a9883487f3c08485de0d7ea047645f85
Himanshu2329 <kumarhimanshu4390@gmail.com>
2023-01-08 00:14:09 +0530

    List Done

--- ListANDTuples/List_methods.py
+++ ListANDTuples/List_methods.py
@@ -1,2 +1,29 @@
 l1=[123,54,234,6,5234,78,1,56,678,23444]
-print(l1.sort())
+l1.sort()
+print(l1)
+
+# remove
+l2=[123,54,234,6,5234,78,1,56,678,23444]
+l2.remove(l2[7])
+print(l2)
+
+# reverse
+l3=[123,54,234,6,5234,78,1,56,678,23444]
+l3.reverse()
+print(l3)
+
+# Apend it means add the element to the last of the list
+l4=[123,54,234,6,5234,78,1,56,678,23444]
+l4.append(812939)
+print(l4)
+
+# insert at specific position
+l5=[123,54,234,6,5234,78,1,56,678,23444]
+l5.insert(3,123213123)
+print(l5)
+
+# pop at specific postion
+l6=[123,54,234,6,5234,78,1,56,678,23444]
+l6.pop(5)
+print(l6)
+
